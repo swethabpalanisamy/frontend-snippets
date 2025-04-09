3aac96134ed6dc19d9411d3fb326085d11985cd9
swethabpalanisamy <swethapalanisamy18@gmail.com>
2025-04-09 12:29:08 +0530

    Dynamic Skeleton Loader in JS

---
+++ Skeleton Loader/script.js
@@ -0,0 +1,28 @@
+function loadSkeleton(skeletonLoader, container, skeletonList = []) {
+  const children = Array.from(container.children);
+  children.forEach((child) => {
+    const { width, height } = child.getBoundingClientRect();
+    console.log(child.getBoundingClientRect());
+    const skeletoncard = document.createElement("div");
+    skeletoncard.className = "skeleton-card";
+    skeletoncard.style.width = `${width}px`;
+    skeletoncard.style.height = `${height}px`;
+    skeletonList.push(skeletoncard);
+    skeletonLoader.appendChild(skeletoncard);
+    // if (child.children.length > 0) {
+    //   console.log(child.children, "child.children");
+    //   Array.from(child.children).forEach((a) => {
+    //     loadSkeleton(skeletonLoader, a, skeletonList);
+    //   });
+    // }
+  });
+  return skeletonList;
+}
+const skeletonLoader = document.querySelector(".skeleton-wrapper");
+const container = document.getElementById("container");
+const skeletonList = loadSkeleton(skeletonLoader, container);
+setTimeout(() => {
+  skeletonList.forEach((s) => s.remove());
+  container.style.visibility = "visible";
+  console.style.position = "static";
+}, 2000);
